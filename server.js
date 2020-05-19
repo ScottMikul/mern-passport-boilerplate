19b21c61cff2c52600d714a3f85ebb4bc8d5f7de
scott mikul <scott.p.mikul@gmail.com>
2020-05-19 13:11:29 -0400

    added cookie testing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 
 const app = express();
@@ -19,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passportBoilerplate");
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passportBoilerplate");
 
 app.post("/api/login",(req,res)=>{
 
@@ -31,9 +32,28 @@ app.post("/api/signup",(req,res)=>{
 
 app.get("/cookie",(req,res)=>{
     //send a cooooookie!
-    res.cookie("COOOOOKIE", "COOOOOOOOOOOOOOOKIEEEEEEEEEEEEEEEEEE", {httpOnly:true})
+    const token = jwt.sign({foo:'bar'},"SUPER SECRET PRIVATE KEY LUL" )
+    res.cookie("COOOOOKIE", token, {httpOnly:true})
+    res.send("sent a cookie nerd");
 })
 
+app.get("/api/seecookie" , (req,res)=>{
+  res.send("checking response for cookies!?");
+})
+
+app.get("/api/cookievalue",(req,res)=>{
+  console.log('Cookies: ', req.cookies)
+  if(req.cookies.COOOOOKIE){
+    var decoded = jwt.verify(req.cookies.COOOOOKIE, "SUPER SECRET PRIVATE KEY LUL")
+    console.log(decoded);
+    console.log(decoded.COOOOOKIE);
+  }
+  res.send("didn't get a cookie value");
+})
+app.get("/api/clearcookie", (req,res)=>{
+  res.clearCookie("COOOOOKIE");
+  res.send("COOOOOKIE was cleared.");
+})
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
